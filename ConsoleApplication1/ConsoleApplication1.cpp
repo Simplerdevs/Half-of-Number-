087bae016c38bb166141bf04f70f20a4f7caa753
Simplerdevs <tplg7839@gmail.com>
2024-06-05 13:53:31 -0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+		/* program to ask the user to enter number then print the half of the inputs */
+
+int main()
+
+{
+	int x;
+	cout << " enter a number" << endl;
+	cin >> x;
+	cout << "the result is" << endl;
+	cout << x / 2 << endl;
+
+	return 0;
+
+		// created by @ilyes_trabelsi
+}
+
